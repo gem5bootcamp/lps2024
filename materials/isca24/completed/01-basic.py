ae484c5e1b04e9ee960f66c15da9fd6b5b90c7c2
Jason Lowe-Power <jason@lowepower.com>
2024-06-25 21:49:52 +0000

    materials: Finish up 01-basic.py
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

---
+++ materials/isca24/completed/01-basic.py
@@ -0,0 +1,9 @@
+from gem5.prebuilt.demo.x86_demo_board import X86DemoBoard
+from gem5.resources.resource import obtain_resource
+from gem5.simulate.simulator import Simulator
+
+board = X86DemoBoard()
+board.set_workload(obtain_resource("x86-ubuntu-24.04-boot-no-systemd"))
+
+simulator = Simulator(board=board)
+simulator.run(20_000_000_000)
