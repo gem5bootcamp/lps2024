e727bf6e56c57ab76b5629042d21ffa4420953c8
Jason Lowe-Power <jason@lowepower.com>
2024-06-26 20:28:08 +0000

    materials: adding multisim example
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

---
+++ materials/isca24/completed/07-multisim.py
@@ -0,0 +1,17 @@
+"""
+A small multisim example
+"""
+
+from gem5.prebuilt.riscvmatched.riscvmatched_board import RISCVMatchedBoard
+from gem5.resources.resource import obtain_resource
+from gem5.simulate.simulator import Simulator
+
+from gem5.utils.multisim import add_simulator
+
+board = RISCVMatchedBoard()
+
+for workload in obtain_resource("riscv-getting-started-benchmark-suite"):
+    board.set_workload(workload)
+    simulator = Simulator(board=board)
+
+    add_simulator(simulator)
