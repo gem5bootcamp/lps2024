86f436d982c7c02c1535de6c2ddb11ba61e43553
Jason Lowe-Power <jason@lowepower.com>
2024-06-26 21:52:12 +0000

    misc: Update pre-download to actually download
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- pre-download-resources.py
+++ pre-download-resources.py
@@ -2,11 +2,11 @@ from gem5.resources.resource import obtain_resource
 
 # grep "obtain_resource(.*)" materials/isca24/ -roh | sort | uniq
 
-obtain_resource("riscv-getting-started-benchmark-suite")
-obtain_resource("x86-linux-kernel-5.4.0-105-generic")
-obtain_resource("x86-m5-exit")
-obtain_resource("x86-npb-is-size-s-run")
-obtain_resource("x86-ubuntu-24.04-boot-no-systemd")
-obtain_resource("x86-ubuntu-24.04-boot-with-systemd")
-obtain_resource("x86-ubuntu-24.04-npb-img")
-# obtain_resource("x86-ubuntu-24.04-npb-is-s-run")
+obtain_resource("riscv-getting-started-benchmark-suite").get_local_path()
+obtain_resource("x86-linux-kernel-5.4.0-105-generic").get_local_path()
+obtain_resource("x86-m5-exit").get_local_path()
+obtain_resource("x86-npb-is-size-s-run").get_local_path()
+obtain_resource("x86-ubuntu-24.04-boot-no-systemd").get_local_path()
+obtain_resource("x86-ubuntu-24.04-boot-with-systemd").get_local_path()
+obtain_resource("x86-ubuntu-24.04-npb-img").get_local_path()
+# obtain_resource("x86-ubuntu-24.04-npb-is-s-run").get_local_path()
