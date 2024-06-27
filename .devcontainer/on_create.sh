8e26b1d7578f86a3adc89f687fc3a5e3a29311d3
Jason Lowe-Power <jason@lowepower.com>
2024-06-27 21:05:24 +0000

    misc: Download GPU files in on_create
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- .devcontainer/on_create.sh
+++ .devcontainer/on_create.sh
@@ -24,3 +24,10 @@ gem5 pre-download-resources.py
 docker pull ghcr.io/gem5/gcn-gpu:v24-0
 
 wget http://dist.gem5.org/dist/v24-0/test-progs/square/square
+
+wget https://storage.googleapis.com/dist.gem5.org/dist/v24-0/gpu-fs/kernel/vmlinux-gpu-ml.gz
+gunzip vmlinux-gpu-ml
+
+# Note: this unzips to 55 GB so must in on /tmp.
+# See post_start.sh where it is unzipped each time the devcontainer starts
+wget https://storage.googleapis.com/dist.gem5.org/dist/v24-0/gpu-fs/diskimage/x86-ubuntu-gpu-ml-isca.gz
---
+++ .devcontainer/post_start.sh
@@ -0,0 +1,7 @@
+
+if [ ! -f /tmp/x86-ubuntu-gpu-ml-isca ]; then
+  cp ~/x86-ubuntu-gpu-ml-isca.gz /tmp
+  gunzip /tmp/x86-ubuntu-gpu-ml-isca.gz
+fi
+
+ln -s /tmp/x86-ubuntu-gpu-ml-isca .
