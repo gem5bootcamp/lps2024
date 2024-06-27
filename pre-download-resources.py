abe10a6f4e9b10a4d760881b763dc77c0d3ba5b9
Jason Lowe-Power <jason@lowepower.com>
2024-06-27 15:45:48 +0000

    misc: Fix the pre-download script
    Signed-off-by: Jason Lowe-Power <jason@lowepower.com>

--- pre-download-resources.py
+++ pre-download-resources.py
@@ -1,12 +1,10 @@
 from gem5.resources.resource import obtain_resource
 
 # grep "obtain_resource(.*)" materials/isca24/ -roh | sort | uniq
+# note, this does not work for workloads. We have to get the resources used
+# in each workload.
 
-obtain_resource("riscv-getting-started-benchmark-suite").get_local_path()
 obtain_resource("x86-linux-kernel-5.4.0-105-generic").get_local_path()
 obtain_resource("x86-m5-exit").get_local_path()
-obtain_resource("x86-npb-is-size-s-run").get_local_path()
-obtain_resource("x86-ubuntu-24.04-boot-no-systemd").get_local_path()
-obtain_resource("x86-ubuntu-24.04-boot-with-systemd").get_local_path()
 obtain_resource("x86-ubuntu-24.04-npb-img").get_local_path()
-# obtain_resource("x86-ubuntu-24.04-npb-is-s-run").get_local_path()
+obtain_resource("x86-ubuntu-24.04-img").get_local_path()
